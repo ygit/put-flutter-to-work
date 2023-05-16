a1e579bee3113403b7cc9dab840f1ea2ae6af044
Yogesh Singh <yogesh.singh.2810@gmail.com>
2023-05-16 21:59:18 +0530

    running Main Flutter Example app

--- newsfeed_ios/newsfeedApp/GeneratedPluginRegistrant.m
+++ newsfeed_ios/newsfeedApp/GeneratedPluginRegistrant.m
@@ -12,17 +12,18 @@
 @import hmssdk_flutter;
 #endif
 
-//#if __has_include(<permission_handler_apple/PermissionHandlerPlugin.h>)
-//#import <permission_handler_apple/PermissionHandlerPlugin.h>
-//#else
-//@import permission_handler_apple;
-//#endif
+#if __has_include(<path_provider_foundation/PathProviderPlugin.h>)
+#import <path_provider_foundation/PathProviderPlugin.h>
+#else
+@import path_provider_foundation;
+#endif
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
   [HmssdkFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"HmssdkFlutterPlugin"]];
-//  [PermissionHandlerPlugin registerWithRegistrar:[registry registrarForPlugin:@"PermissionHandlerPlugin"]];
+  [PathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"PathProviderPlugin"]];
 }
 
 @end
+
